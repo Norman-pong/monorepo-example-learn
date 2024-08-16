65980e578f123c05c95350256d1eb9fca14434f1
Norman Pong <zhiming3399@gmail.com>
2024-08-16 10:14:21 +0800

    chore(media-info): 新增 index 文件

--- packages/media-info/getMedia.mjs
+++ packages/media-info/getMedia.mjs
@@ -1,18 +1,20 @@
 import ffmpeg from 'fluent-ffmpeg';
 import fs from 'node:fs'
 
-fs.readdirSync('./assets', { withFileTypes: true, recursive: true }).forEach(file => {
-  const { name } = file
-  if (!file.isFile()) return
-  ffmpeg.ffprobe(`./assets/${name}`, function (err, metadata) {
-    if (err) {
-      console.error('Not Media File:', err);
-    } else {
-      if (!fs.existsSync('./out')) {
-        fs.mkdirSync('./out');
+export default function () {
+  fs.readdirSync('./assets', { withFileTypes: true, recursive: true }).forEach(file => {
+    const { name } = file
+    if (!file.isFile()) return
+    ffmpeg.ffprobe(`./assets/${name}`, function (err, metadata) {
+      if (err) {
+        console.error('Not Media File:', err);
+      } else {
+        if (!fs.existsSync('./out')) {
+          fs.mkdirSync('./out');
+        }
+        fs.writeFileSync(`./out/metadata-${name}.json`, JSON.stringify(metadata,
+          null, 2));
       }
-      fs.writeFileSync(`./out/metadata-${name}.json`, JSON.stringify(metadata,
-        null, 2));
-    }
-  });
-});
+    });
+  })
+}
---
+++ packages/media-info/index.mjs
@@ -0,0 +1,3 @@
+import main from './getMedia.mjs';
+
+main();
